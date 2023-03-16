655fcc8320a33d4e75e45b8499ec59b6e3b9aa22
Milton Carlos Katoo <mckatoo@gmail.com>
2023-03-16 17:05:17 -0300

    change to optional properties

--- src/domain/entities/AboutPage.ts
+++ src/domain/entities/AboutPage.ts
@@ -4,8 +4,8 @@ export default class AboutPage {
   constructor(
     readonly title: string,
     readonly description: string,
-    readonly skills: Skill,
-    readonly illustrationURL: string,
-    readonly illustrationALT: string,
+    readonly skills: Skill[],
+    readonly illustrationURL?: string,
+    readonly illustrationALT?: string,
   ) {}
 }
