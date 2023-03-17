0639eacbb488372a816a3e3e2837072022ea3f2a
Milton Carlos Katoo <mckatoo@gmail.com>
2023-03-17 15:06:24 -0300

    remove mock from inMemoryDB

--- src/infra/db/InMemory/AboutPage/AboutPageInMemory.ts
+++ src/infra/db/InMemory/AboutPage/AboutPageInMemory.ts
@@ -1,8 +1,7 @@
 import AboutPage from "@/domain/entities/AboutPage";
 import IAboutPage from "@/domain/repository/IAboutPage";
-import aboutPageMock from "@/mock/aboutPageMock";
 
-let aboutPage: AboutPage[] = [aboutPageMock];
+let aboutPage: AboutPage[] = [];
 
 export default class AboutPageInMemory implements IAboutPage {
   async createAboutPage(page: AboutPage): Promise<void> {
