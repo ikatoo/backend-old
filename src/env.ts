0f4e04270bd5991429e5e7e3b7dae8bd74b354fe
Milton Carlos Katoo <mckatoo@gmail.com>
2023-03-20 18:39:23 -0300

    update environment variables

--- src/env.ts
+++ src/env.ts
@@ -7,11 +7,16 @@ const getEnv = (key: string) => {
     console.error(`*** ${key} IS UNDEFINED. ***`);
     process.exit(1)
   }
-  
+
   return `${process.env[key] ?? ""}`;
 };
 
 export const env = {
-  PORT: +(getEnv("PORT")),
+  PORT: +getEnv("PORT"),
   HOST: (getEnv("HOST")),
+  POSTGRES_PASSWORD: getEnv("POSTGRES_PASSWORD"),
+  POSTGRES_USER: getEnv("POSTGRES_USER"),
+  POSTGRES_HOSTNAME: getEnv("POSTGRES_HOSTNAME"),
+  POSTGRES_PORT: +getEnv("POSTGRES_PORT"),
+  POSTGRES_DBNAME: getEnv("POSTGRES_DBNAME"),
 };
