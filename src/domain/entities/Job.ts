89673045f176f63af942afe2a887874892f256a5
Milton Carlos Katoo <mckatoo@gmail.com>
2023-03-21 12:15:07 -0300

    add title on jobs

--- src/domain/entities/Job.ts
+++ src/domain/entities/Job.ts
@@ -3,6 +3,7 @@ import Skill from "./Skill";
 export default class Job {
   constructor(
     readonly id: number,
+    readonly title: string,
     readonly description: string,
     readonly skills: Skill[],
     readonly link: string,
