79d69af4912703d063990fb2c7748a6d871d16b3
Milton Carlos Katoo <mckatoo@gmail.com>
2023-03-21 12:15:07 -0300

    add title on jobs

---
+++ src/domain/repository/IJobs.ts
@@ -0,0 +1,16 @@
+export type JobIn = {
+  title: string
+  description: string
+  link: string
+  start: number
+  end?: number
+};
+
+export type JobOut = JobIn & { id: number };
+
+export default interface IJobs {
+  createJob(job: JobIn): Promise<void>;
+  getJobs(): Promise<JobOut[]>;
+  updateJob(id: number, job: Partial<JobIn>): Promise<void>;
+  deleteJob(id: number): Promise<void>;
+}
