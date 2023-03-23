0fc08141ac66e87f014fc7990cf849766b1b6988
Milton Carlos Katoo <mckatoo@gmail.com>
2023-03-22 22:21:01 -0300

    implemented projects on postgres database

--- src/domain/repository/IProject.ts
+++ src/domain/repository/IProject.ts
@@ -3,8 +3,8 @@ import Project from "../entities/Project";
 export type ProjectIn = Omit<Project, 'id'>
 
 export default interface IProjects {
-  createProject(job: ProjectIn): Promise<void>;
+  createProject(project: ProjectIn): Promise<void>;
   getProjects(): Promise<Project[]>;
-  updateProject(id: number, job: Partial<ProjectIn>): Promise<void>;
+  updateProject(id: number, project: Partial<ProjectIn>): Promise<void>;
   deleteProject(id: number): Promise<void>;
 }
