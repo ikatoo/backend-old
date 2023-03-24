f9a2b425dec131e3b43c7c105dfd071773099b65
Milton Carlos Katoo <mckatoo@gmail.com>
2023-03-24 18:59:38 -0300

    change localization_lat and localization_lng to localization object

--- src/domain/entities/Contact.ts
+++ src/domain/entities/Contact.ts
@@ -1,8 +1,12 @@
+type Localization = {
+  latitude: number
+  longitude: number
+}
+
 export default class Contact {
   constructor(
     readonly id: number,
     readonly email: string,
-    readonly localization_lat?: number,
-    readonly localization_lng?: number
+    readonly localization?: Localization,
   ) { }
 }
