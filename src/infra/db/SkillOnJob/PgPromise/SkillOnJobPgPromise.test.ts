ea284afb7c8a8c0d36f4b7adddd87f49023c2754
Milton Carlos Katoo <mckatoo@gmail.com>
2023-03-26 11:06:49 -0300

    add skills_on_jobs table with implementation and tests

---
+++ src/infra/db/SkillOnJob/PgPromise/SkillOnJobPgPromise.test.ts
@@ -0,0 +1,78 @@
+import { randomInt } from "crypto";
+import { beforeEach, describe, expect, test } from "vitest";
+import db from "./db";
+import SkillOnJobPgPromise from "./SkillOnJobPgPromise";
+
+describe('Basic operations in Skills_Jobs PgPromise Database', () => {
+  const skillsOnJobsRepository = new SkillOnJobPgPromise()
+
+  beforeEach(() => {
+    db.none('delete from skills_jobs;')
+  })
+
+  test('CREATE Method', async () => {
+    const skillOnJobMock = {
+      skillId: randomInt(1, 10),
+      jobId: randomInt(1, 10)
+    }
+
+    await expect(skillsOnJobsRepository.createSkillOnJob(skillOnJobMock))
+      .resolves.not.toThrow()
+  })
+
+  test('READ Method', async () => {
+    const skillOnJobMock = {
+      skillId: randomInt(1, 10),
+      jobId: randomInt(1, 10)
+    }
+    await skillsOnJobsRepository.createSkillOnJob(skillOnJobMock)
+    const expected = [skillOnJobMock]
+    const actual = await skillsOnJobsRepository.getJobsBySkillId(skillOnJobMock.skillId)
+
+    expect(expected).toEqual(actual)
+  })
+
+  test('UPDATE Method', async () => {
+    const oldValue = {
+      skillId: randomInt(1, 10),
+      jobId: randomInt(1, 10)
+    }
+    await skillsOnJobsRepository.createSkillOnJob(oldValue)
+
+    const newValue = {
+      skillId: randomInt(1, 10),
+      jobId: randomInt(1, 10)
+    }
+    await skillsOnJobsRepository.updateSkillOnJob(oldValue, newValue)
+  })
+
+  test('NOT update with existent value', async () => {
+    const skillOnJob = {
+      skillId: randomInt(1, 10),
+      jobId: randomInt(1, 10)
+    }
+    await skillsOnJobsRepository.createSkillOnJob(skillOnJob)
+
+    await expect(skillsOnJobsRepository.updateSkillOnJob(skillOnJob, skillOnJob))
+      .rejects.toThrowError('Relation between skill and job already exists.')
+  })
+
+  test('DELETE Method', async () => {
+    const skillOnJob = {
+      skillId: randomInt(1, 10),
+      jobId: randomInt(1, 10)
+    }
+    await skillsOnJobsRepository.createSkillOnJob(skillOnJob)
+    await skillsOnJobsRepository.deleteSkillOnJob(skillOnJob)
+    const actual = await db.oneOrNone(
+      'select * from skills_jobs where skill_id = $1 and job_id = $2',
+      [
+        skillOnJob.skillId,
+        skillOnJob.jobId
+      ]
+    )
+
+    expect(actual).toBeNull()
+  })
+
+})
---
+++ src/infra/db/SkillOnJob/PgPromise/SkillOnJobPgPromise.ts
@@ -0,0 +1,72 @@
+import { ISkillOnJob, SkillOnJob } from "@/domain/repository/ISkillOnJob"
+import db from "./db"
+
+export default class SkillOnJobPgPromise implements ISkillOnJob {
+
+  async getSkillsByJobId(jobId: number): Promise<SkillOnJob[]> {
+    const skillsJobs = await db.manyOrNone(
+      'select * from skills_jobs where job_id = $1',
+      jobId
+    ) ?? {}
+    const mappedSkillsJobs = skillsJobs.map(skillJob => ({
+      jobId: skillJob.job_id,
+      skillId: skillJob.skill_id
+    }))
+
+    return mappedSkillsJobs
+  }
+
+  async getJobsBySkillId(skillId: number): Promise<SkillOnJob[]> {
+    const skillsJobs = await db.manyOrNone(
+      'select * from skills_jobs where skill_id = $1',
+      skillId
+    ) ?? {}
+    const mappedSkillsJobs = skillsJobs.map(skillJob => ({
+      jobId: skillJob.job_id,
+      skillId: skillJob.skill_id
+    }))
+
+    return mappedSkillsJobs
+  }
+
+  async createSkillOnJob(skillOnJob: SkillOnJob) {
+    await db.none(
+      `insert into skills_jobs (
+        job_id, skill_id
+      ) values ($1,$2);`,
+      [
+        skillOnJob.jobId,
+        skillOnJob.skillId
+      ],
+    )
+  }
+
+  async updateSkillOnJob(oldValue: SkillOnJob, newValue: SkillOnJob): Promise<void> {
+    const exists = await db.oneOrNone(
+      'select * from skills_jobs where skill_id = $1 and job_id = $2;',
+      [newValue.skillId, newValue.jobId]
+    )
+    if(exists) throw new Error("Relation between skill and job already exists.");
+    
+    
+    const query = `update skills_jobs set skill_id = $1, job_id = $2 
+      where skill_id = $3 and job_id = $4;`
+    await db.none(query, [
+      newValue.skillId,
+      newValue.jobId,
+      oldValue.skillId,
+      oldValue.jobId
+    ]);
+  }
+
+  async deleteSkillOnJob(skillOnJob: SkillOnJob): Promise<void> {
+    await db.none(
+      `delete from skills_jobs where skill_id = $1 and job_id = $2`,
+      [
+        skillOnJob.skillId,
+        skillOnJob.jobId
+      ]
+    )
+  }
+
+}
