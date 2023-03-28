7ca79078c8a0f36bf23640c433dcd9cfc896c49a
Milton Carlos Katoo <mckatoo@gmail.com>
2023-03-28 14:01:58 -0300

    add  clear table after each test

--- src/infra/http/controllers/aboutPage/aboutPageController.test.ts
+++ src/infra/http/controllers/aboutPage/aboutPageController.test.ts
@@ -1,11 +1,17 @@
 import { AboutPageRepository } from "@/infra/db";
+import db from "@/infra/db/PgPromise";
 import aboutPageMock from "@/mock/aboutPageMock";
-import { describe, expect, test } from "vitest";
+import { afterEach, describe, expect, test } from "vitest";
 import { getAboutPageHandler } from "./aboutPageController";
 
 describe("AboutPage Controller test", () => {
+  const aboutPageRepository = new AboutPageRepository();
+
+  afterEach(async () => {
+    await db.none('delete from about_page;')
+  })
+
   test("Get about page data", async () => {
-    const aboutPageRepository = new AboutPageRepository();
     await aboutPageRepository.createAboutPage(aboutPageMock);
     const result = await getAboutPageHandler();
 
