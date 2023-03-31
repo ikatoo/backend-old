b0411b6a8e8a7db480eb748a3a857bbd78b910d6
Milton Carlos Katoo <mckatoo@gmail.com>
2023-03-31 09:58:41 -0300

    remove duplicate type

--- src/infra/http/controllers/skillsPage/skillsPageController.ts
+++ src/infra/http/controllers/skillsPage/skillsPageController.ts
@@ -1,29 +1,9 @@
 import { SkillsPageRepository } from "@/infra/db";
-import { dateNumberToString, dateToString } from "@/utils/transformers/dateTransform";
+import { SkillsPage } from "@/repository/ISkillsPage";
 
-type SkillsOutput = {
-  skillTitle: string
-  rankPercent: number
-}
+const skillsPageRepository = new SkillsPageRepository();
 
-type JobsOutput = {
-  jobTitle: string,
-  jobDescription: string
-  yearMonthStart: string
-  yearMonthEnd?: string
-  link: string
-}
-
-type SkillsPageOutput = {
-  title: string,
-  description: string,
-  skills: SkillsOutput[],
-  lastJobs: JobsOutput[]
-}
-
-async function getSkillsPageHandler(): Promise<SkillsPageOutput | undefined> {
-  const skillsPageRepository = new SkillsPageRepository();
-
+async function getSkillsPageHandler(): Promise<SkillsPage | undefined> {
   const skillsPage = await skillsPageRepository.getSkillsPage()
   if (!skillsPage) return undefined
 
