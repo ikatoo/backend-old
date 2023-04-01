c3f20ffe6bae04ec7384957c46e4ebbf32a80856
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-01 15:21:08 -0300

    impliced type

--- src/infra/db/PgPromise/Projects/ProjectsPgPromise.test.ts
+++ src/infra/db/PgPromise/Projects/ProjectsPgPromise.test.ts
@@ -1,4 +1,5 @@
 import projectsPageMock from "@/mock/projectsPageMock";
+import { Project } from "@/repository/IProject";
 import { dateToString } from "@/utils/transformers/dateTransform";
 import { afterEach, describe, expect, test } from "vitest";
 import db from "..";
@@ -39,7 +40,7 @@ describe("Basic operations in Projects Postgres Database", () => {
       'select * from projects where title = $1',
       selectedProject.description.title
     )
-    const { id, ...actual } = await repository.getProjectById(project.id)
+    const { id, ...actual } = await repository.getProjectById(project.id) as Project
 
     expect(selectedProject).toEqual(actual)
   })
@@ -52,7 +53,7 @@ describe("Basic operations in Projects Postgres Database", () => {
       'select * from projects where title = $1',
       selectedProject.description.title
     )
-    const { id, ...actual } = await repository.getProjectByTitle(project.title)
+    const { id, ...actual } = await repository.getProjectByTitle(project.title) as Project
 
     expect(selectedProject).toEqual(actual)
   })
