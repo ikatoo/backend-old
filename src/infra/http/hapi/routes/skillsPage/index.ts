b55eb561dc640c853d0a022e186b855a0a02aac4
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-01 10:19:28 -0300

    add routes and tests for skills page

--- src/infra/http/hapi/routes/skillsPage/index.ts
+++ src/infra/http/hapi/routes/skillsPage/index.ts
@@ -1,13 +1,72 @@
-import { ReqRefDefaults, ServerRoute } from "@hapi/hapi";
 import { getSkillsPageHandler } from "@/infra/http/controllers";
+import { createSkillsPageHandler, updateSkillsPageHandler } from "@/infra/http/controllers/skillsPage/skillsPageController";
+import { PartialSkillsPageSchema, SkillsPageSchema } from "@/repository/ISkillsPage";
+import { ReqRefDefaults, ServerRoute } from "@hapi/hapi";
 const skillsPageRoutes: ServerRoute<
   ReqRefDefaults
 >[] = [
-  {
-    method: "GET",
-    path: "/skills",
-    handler: getSkillsPageHandler,
-  },
-];
+    {
+      method: "GET",
+      path: "/skills",
+      handler: async (_request, h) => {
+        const page = await getSkillsPageHandler()
+        if (!page) {
+          return h.response().code(404)
+        }
+        return h.response(page)
+      },
+    },
+    {
+      method: "PUT",
+      path: "/skills",
+      handler: async (_request, h) => {
+        return h.response().code(405)
+      }
+    },
+    {
+      method: "POST",
+      path: "/skills",
+      handler: async (request, h) => {
+        if (!request.payload) return h.response().code(400)
+
+        const validPage = SkillsPageSchema.safeParse(request.payload)
+        if (!validPage.success)
+          return h.response({
+            error: 'Invalid type.'
+          }).code(409)
+        try {
+          await createSkillsPageHandler(validPage.data)
+          return h.response().code(204)
+        } catch (error) {
+          if (error instanceof Error && error.message.includes('duplicate'))
+            return h.response({
+              error: error.message
+            }).code(409)
+        }
+      }
+    },
+    {
+      method: "PATCH",
+      path: "/skills",
+      handler: async (request, h) => {
+        if (!request.payload) return h.response().code(400)
+
+        const validPage = PartialSkillsPageSchema.safeParse(request.payload)
+        if (!validPage.success || Object.keys(validPage.data).length === 0)
+          return h.response({
+            error: 'Invalid type.'
+          }).code(409)
+        try {
+          await updateSkillsPageHandler(validPage.data)
+          return h.response().code(204)
+        } catch (error) {
+          if (error instanceof Error && error.message.includes('duplicate'))
+            return h.response({
+              error: error.message
+            }).code(409)
+        }
+      }
+    }
+  ];
 
 export default skillsPageRoutes;
