9c56d476ea72c3b8f032868e6096816f4e70cf1c
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-10 08:20:31 -0300

    move validation to controller

--- src/infra/http/controllers/aboutPage/aboutPageController.ts
+++ src/infra/http/controllers/aboutPage/aboutPageController.ts
@@ -1,5 +1,8 @@
 import { AboutPageRepository } from "@/infra/db";
-import { AboutPage } from "@/repository/IAboutPage";
+import { AboutPage, AboutPageSchema } from "@/repository/IAboutPage";
+
+export type StatusCode = { statusCode?: number }
+export type AboutPageController = (page: AboutPage) => Promise<StatusCode>
 
 const aboutPageRepository = new AboutPageRepository();
 
@@ -9,8 +12,25 @@ async function getAboutPageHandler() {
   return aboutPage
 }
 
-async function createAboutPageHandler(page: AboutPage) {
-  await aboutPageRepository.createAboutPage(page)
+async function createAboutPageHandler(page: AboutPage): Promise<ControllerResponse> {
+  const validPage = AboutPageSchema.safeParse(page)
+  if (!validPage.success)
+    return {
+      error: 'Invalid type.',
+      statusCode: 409
+    }
+
+  try {
+    await aboutPageRepository.createAboutPage(page)
+    return { statusCode: 201 }
+  } catch (error) {
+    if (error instanceof Error && error.message.includes('duplicate'))
+      return {
+        error: error.message,
+        statusCode: 409
+      }
+    return { statusCode: 500 }
+  }
 }
 
 async function updateAboutPageHandler(page: Partial<AboutPage>) {
