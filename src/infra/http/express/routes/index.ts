ebb6e6ebbf32b55f8fc8c7822e36657619f4f17d
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-12 19:10:59 -0300

    implemented express routes to contact page

--- src/infra/http/express/routes/index.ts
+++ src/infra/http/express/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { aboutRoutes } from './about'
+import { contactRoutes } from './contactPage'
 
 const routes = Router()
 
 routes.use(aboutRoutes)
+routes.use(contactRoutes)
 
 export { routes }
