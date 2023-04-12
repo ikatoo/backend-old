43a08452ea9036d25897189e9fb11d3c41a6a044
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-12 13:07:10 -0300

    merge request parameters

--- src/infra/http/adapters/hapiAdapter.ts
+++ src/infra/http/adapters/hapiAdapter.ts
@@ -2,7 +2,9 @@ import { Request, ResponseToolkit } from "@hapi/hapi"
 
 export default (handler: HandlerFunction) => {
   const hapiHandler = async (request: Request, h: ResponseToolkit) => {
-    const page = request.payload ?? request.params
+    const page = typeof request.payload === "object"
+      ? { ...request.payload, ...request.params }
+      : request.params
     const { statusCode, error, body } = await handler({ parameters: page })
     if (error) {
       return statusCode
