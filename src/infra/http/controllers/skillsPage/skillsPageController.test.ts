39ea5a59b553e8981a7fc914ab62ddc78928785e
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-12 15:01:13 -0300

    implemented hapi adapter on skills page routes

--- src/infra/http/controllers/skillsPage/skillsPageController.test.ts
+++ src/infra/http/controllers/skillsPage/skillsPageController.test.ts
@@ -12,38 +12,39 @@ describe("SkillsPage Controller test", () => {
 
   test("Get skills page data", async () => {
     await skillsPageRepository.createSkillsPage(skillPageMock);
-    const result = await getSkillsPageHandler();
+    const { body, statusCode } = await getSkillsPageHandler();
 
-    expect(skillPageMock).toEqual(result);
+    expect(body).toEqual(skillPageMock);
+    expect(statusCode).toEqual(200);
   });
 
   test("Create skills page data without error", async () => {
-    await expect(createSkillsPageHandler(skillPageMock))
+    await expect(createSkillsPageHandler({ parameters: skillPageMock }))
       .resolves.not.toThrow()
     const page = await skillsPageRepository.getSkillsPage()
 
     expect(page).toEqual(skillPageMock)
   });
 
-  test("Update skills page data without error", async () => {
+  test("Update skills page data with 204 status code", async () => {
     await skillsPageRepository.createSkillsPage(skillPageMock);
     const newData = {
       title: 'new title',
       description: 'new Description'
     }
-    await expect(updateSkillsPageHandler(newData))
-      .resolves.not.toThrow()
+    const { statusCode } = await updateSkillsPageHandler({ parameters: newData })
     const page = await skillsPageRepository.getSkillsPage()
 
+    expect(statusCode).toEqual(204)
     expect(page).toEqual({ ...skillPageMock, ...newData })
   });
 
-  test("Delete skills page data", async () => {
+  test("Delete skills page data with 204 status code", async () => {
     await skillsPageRepository.createSkillsPage(skillPageMock);
-    await expect(deleteSkillsPageHandler())
-      .resolves.not.toThrow()
+    const { statusCode } = await deleteSkillsPageHandler()
     const page = await skillsPageRepository.getSkillsPage()
 
+    expect(statusCode).toEqual(204)
     expect(page).toBeUndefined()
   });
 });
--- src/infra/http/controllers/skillsPage/skillsPageController.ts
+++ src/infra/http/controllers/skillsPage/skillsPageController.ts
@@ -1,25 +1,76 @@
 import { SkillsPageRepository } from "@/infra/db";
-import { SkillsPage } from "@/repository/ISkillsPage";
+import { PartialSkillsPageSchema, SkillsPageSchema } from "@/repository/ISkillsPage";
 
 const skillsPageRepository = new SkillsPageRepository();
 
-async function getSkillsPageHandler(): Promise<SkillsPage | undefined> {
-  const skillsPage = await skillsPageRepository.getSkillsPage()
-  if (!skillsPage) return
-
-  return skillsPage
+async function getSkillsPageHandler(): Promise<HandlerResponse> {
+  const pageData = await skillsPageRepository.getSkillsPage()
+  if (!pageData) {
+    return { statusCode: 204 }
+  }
+  return {
+    body: pageData,
+    statusCode: 200
+  }
 }
 
-async function createSkillsPageHandler(page: SkillsPage): Promise<void> {
-  await skillsPageRepository.createSkillsPage(page)
+async function createSkillsPageHandler(handlerProps?: HandlerProps): Promise<HandlerResponse> {
+  if (!Object.keys(handlerProps?.parameters!).length) return {
+    statusCode: 400
+  }
+
+  const validPage = SkillsPageSchema.safeParse(handlerProps?.parameters)
+  if (!validPage.success)
+    return {
+      error: 'Invalid type.',
+      statusCode: 409
+    }
+  try {
+    await skillsPageRepository.createSkillsPage(validPage.data)
+    return { statusCode: 201 }
+  } catch (error) {
+    return {
+      error: error instanceof Error ? error.message : undefined,
+      statusCode: 409
+    }
+  }
 }
 
-async function updateSkillsPageHandler(page: Partial<SkillsPage>): Promise<void> {
-  await skillsPageRepository.updateSkillsPage(page)
+async function updateSkillsPageHandler(handlerProps?: HandlerProps): Promise<HandlerResponse> {
+  if (!Object.keys(handlerProps?.parameters!).length) return {
+    statusCode: 400
+  }
+
+  const validPage = PartialSkillsPageSchema.safeParse(handlerProps?.parameters)
+  if (!validPage.success || Object.keys(validPage.data).length === 0)
+    return {
+      error: 'Invalid type.',
+      statusCode: 409
+    }
+  try {
+    await skillsPageRepository.updateSkillsPage(validPage.data)
+    return {
+      statusCode: 204
+    }
+  } catch (error) {
+    return {
+      error: error instanceof Error ? error.message : undefined,
+      statusCode: 409
+    }
+  }
 }
 
-async function deleteSkillsPageHandler(): Promise<void> {
-  await skillsPageRepository.deleteSkillsPage()
+async function deleteSkillsPageHandler(): Promise<HandlerResponse> {
+  try {
+    await skillsPageRepository.deleteSkillsPage()
+    return { statusCode: 204 }
+  } catch (error) {
+    return {
+      error: error instanceof Error ? error.message : undefined,
+      statusCode: 500
+    }
+  }
+
 }
 
 export {
