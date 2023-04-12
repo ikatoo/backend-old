8bb7b797655c498ebfd4b209b8d625ffbcab61f7
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-12 15:01:13 -0300

    implemented hapi adapter on skills page routes

--- src/infra/http/hapi/routes/skillsPage/index.ts
+++ src/infra/http/hapi/routes/skillsPage/index.ts
@@ -1,6 +1,6 @@
+import hapiAdapter from "@/infra/http/adapters/hapiAdapter";
 import { getSkillsPageHandler } from "@/infra/http/controllers";
 import { createSkillsPageHandler, deleteSkillsPageHandler, updateSkillsPageHandler } from "@/infra/http/controllers/skillsPage/skillsPageController";
-import { PartialSkillsPageSchema, SkillsPageSchema } from "@/repository/ISkillsPage";
 import { ReqRefDefaults, ServerRoute } from "@hapi/hapi";
 const skillsPageRoutes: ServerRoute<
   ReqRefDefaults
@@ -8,13 +8,7 @@ const skillsPageRoutes: ServerRoute<
     {
       method: "GET",
       path: "/skills",
-      handler: async (_request, h) => {
-        const pageData = await getSkillsPageHandler()
-        if (!pageData) {
-          return h.response().code(204)
-        }
-        return h.response(pageData)
-      },
+      handler: hapiAdapter(getSkillsPageHandler)
     },
     {
       method: "PUT",
@@ -26,54 +20,17 @@ const skillsPageRoutes: ServerRoute<
     {
       method: "POST",
       path: "/skills",
-      handler: async (request, h) => {
-        if (!request.payload) return h.response().code(400)
-
-        const validPage = SkillsPageSchema.safeParse(request.payload)
-        if (!validPage.success)
-          return h.response({
-            error: 'Invalid type.'
-          }).code(409)
-        try {
-          await createSkillsPageHandler(validPage.data)
-          return h.response().code(201)
-        } catch (error) {
-          if (error instanceof Error && error.message.includes('duplicate'))
-            return h.response({
-              error: error.message
-            }).code(409)
-        }
-      }
+      handler: hapiAdapter(createSkillsPageHandler)
     },
     {
       method: "PATCH",
       path: "/skills",
-      handler: async (request, h) => {
-        if (!request.payload) return h.response().code(400)
-
-        const validPage = PartialSkillsPageSchema.safeParse(request.payload)
-        if (!validPage.success || Object.keys(validPage.data).length === 0)
-          return h.response({
-            error: 'Invalid type.'
-          }).code(409)
-        try {
-          await updateSkillsPageHandler(validPage.data)
-          return h.response().code(204)
-        } catch (error) {
-          if (error instanceof Error && error.message.includes('duplicate'))
-            return h.response({
-              error: error.message
-            }).code(409)
-        }
-      }
+      handler: hapiAdapter(updateSkillsPageHandler)
     },
     {
       method: "DELETE",
       path: '/skills',
-      handler: async (_request, h) => {
-        await deleteSkillsPageHandler()
-        return h.response().code(204)
-      }
+      handler: hapiAdapter(deleteSkillsPageHandler)
     }
   ];
 
