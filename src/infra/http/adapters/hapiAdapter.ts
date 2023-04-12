9e1b7cd352c674d675a561676245e8f7e3102378
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-12 17:50:39 -0300

    change const name

--- src/infra/http/adapters/hapiAdapter.ts
+++ src/infra/http/adapters/hapiAdapter.ts
@@ -2,10 +2,10 @@ import { Request, ResponseToolkit } from "@hapi/hapi"
 
 export default (handler: HandlerFunction) => {
   const hapiHandler = async (request: Request, h: ResponseToolkit) => {
-    const page = typeof request.payload === "object"
+    const parameters = typeof request.payload === "object"
       ? { ...request.payload, ...request.params }
       : request.params
-    const { statusCode, error, body } = await handler({ parameters: page })
+    const { statusCode, error, body } = await handler({ parameters })
     if (error) {
       return statusCode
         ? h.response({ error }).code(statusCode)
