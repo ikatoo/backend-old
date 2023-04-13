df936ea43ee504fef7be8ce23033a6d8b9d377de
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-12 22:57:03 -0300

    implemented express routes to skills page

--- src/infra/http/express/routes/index.ts
+++ src/infra/http/express/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import { aboutRoutes } from './about'
 import { contactRoutes } from './contactPage'
 import { projectsRoutes } from './projectsPage'
+import { skillsRoutes } from './skillsPage'
 
 const routes = Router()
 
 routes.use(aboutRoutes)
 routes.use(contactRoutes)
 routes.use(projectsRoutes)
+routes.use(skillsRoutes)
 
 export { routes }
