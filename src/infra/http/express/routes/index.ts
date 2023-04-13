f405a219fef1526ae56d490020b8f918b22841af
Milton Carlos Katoo <mckatoo@gmail.com>
2023-04-12 21:07:46 -0300

    implemented express routes to projects

--- src/infra/http/express/routes/index.ts
+++ src/infra/http/express/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { aboutRoutes } from './about'
 import { contactRoutes } from './contactPage'
+import { projectsRoute } from './projectsPage'
 
 const routes = Router()
 
 routes.use(aboutRoutes)
 routes.use(contactRoutes)
+routes.use(projectsRoute)
 
 export { routes }
