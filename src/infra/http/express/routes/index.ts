39f0efd821beaef85078b8b4a70a666b49e7f362
Milton Carlos Katoo <mckatoo@gmail.com>
2023-06-08 21:03:19 -0300

    read package.json for get version

--- src/infra/http/express/routes/index.ts
+++ src/infra/http/express/routes/index.ts
@@ -1,15 +1,17 @@
-import 'express-async-errors'
 import { Router } from 'express'
+import 'express-async-errors'
+import { readFile } from 'fs/promises'
 import { aboutRoutes } from './about'
 import { contactRoutes } from './contactPage'
 import { projectsRoutes } from './projectsPage'
 import { skillsRoutes } from './skillsPage'
-import { version } from 'package.json';
 
 const routes = Router()
 
 routes.get('/', async (_request, response) => {
-  response.json({ version })
+  const packageString = await readFile('./package.json')
+  const packageJson = JSON.parse(packageString.toString('utf-8'))
+  response.json({ version: packageJson.version })
 })
 routes.use(aboutRoutes)
 routes.use(contactRoutes)
