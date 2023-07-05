3ab66d66f28a4196b25e66ea9f249fe756e2edcc
Milton Carlos Katoo <mckatoo@gmail.com>
2023-07-04 23:48:39 -0300

    added morgan and multer dependency and start morgan

--- src/infra/http/express/server.ts
+++ src/infra/http/express/server.ts
@@ -1,10 +1,16 @@
-import cors from 'cors'
-import express, { NextFunction, Request, Response } from 'express'
+import cors from 'cors';
+import express from 'express';
 
-import { routes } from './routes'
-import { errorMiddleware } from './middlewares/errorMiddleware'
+import { env } from '@/utils/env';
+import morgan from 'morgan';
+import { errorMiddleware } from './middlewares/errorMiddleware';
+import { routes } from './routes';
 
 const app = express()
+
+env.NODE_ENV.toLowerCase().startsWith('dev')
+  ? app.use(morgan('dev'))
+  : app.use(morgan('combined'))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -17,4 +23,4 @@ app.use(routes)
 
 app.use(errorMiddleware)
 
-export { app }
+export { app };
