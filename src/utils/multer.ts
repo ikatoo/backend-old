7db68b099b5f2f7eeff13b6ef0733871e9bcfe95
Milton Carlos Katoo <mckatoo@gmail.com>
2023-07-09 11:31:30 -0300

    increase limit of the file size and filter image by mimetype

--- src/utils/multer.ts
+++ src/utils/multer.ts
@@ -1,15 +1,15 @@
 import multer from "multer";
 import path from "path";
+import { BadRequestError } from "./httpErrors";
 
 export default multer({
   storage: multer.diskStorage({}),
   limits: {
-    fileSize: 2 * 1024 * 1024
+    fileSize: 4 * 1024 * 1024
   },
   fileFilter: (_request, file, callbackFn) => {
-    let ext = path.extname(file.originalname);
-    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
-      callbackFn(new Error("File type is not supported"))
+    if (!file.mimetype.startsWith('image/')) {
+      callbackFn(new BadRequestError("File type is not supported"))
       return;
     }
     callbackFn(null, true);
