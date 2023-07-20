56d723a15b2a5413f4dd1e52a4a20ff3683dd919
Milton Carlos Katoo <mckatoo@gmail.com>
2023-07-20 13:26:03 -0300

    ✨ (feature): Init mailer implementation

---
+++ src/repository/IMail.ts
@@ -0,0 +1,14 @@
+import { z } from "zod"
+
+const MailSchema = z.object({
+  from: z.string().email('is not valid email format'),
+  to: z.string().email('is not valid email format'),
+  subject: z.string(),
+  message: z.string(),
+})
+
+export type Mail = z.infer<typeof MailSchema>
+
+export interface IMail {
+  send(mail: Mail): Promise<void>
+}
