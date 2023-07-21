2d9c499f3dcf10185eec0b4e3dbd7b04c1b7d8aa
Milton Carlos Katoo <mckatoo@gmail.com>
2023-07-21 00:04:45 -0300

    test mailer service

--- src/infra/mailer/mailer.test.ts
+++ src/infra/mailer/mailer.test.ts
@@ -1,11 +1,33 @@
-import { describe, test } from "vitest"
+import { env } from "@/utils/env";
+import nodemailer from 'nodemailer';
+import { describe, expect, test, vi } from "vitest";
+import NodeMailerImplementation from "./nodemailer";
 
 describe('Mailer:', () => {
-  test.todo('should sucess on send email', () => {
+  const mailer = new NodeMailerImplementation()
 
+  test('should sucess on send email', async () => {
+    const {accepted, response} = await mailer.send({
+      from: 'from@email.com',
+      to: 'to@email.com',
+      subject: 'subject test',
+      message: '<p>message</p>'
+    })
+
+    expect(accepted).toBeTruthy()
+    expect(response.startsWith('250 Accepted [STATUS=new MSGID='))
   })
-  
-  test.todo('should fail on send mail', () => {
 
+  test('should fail on send mail', async () => {
+    nodemailer.createTransport = vi.fn().mockResolvedValue({
+      sendMail: vi.fn().mockResolvedValue({})
+    })
+
+    await expect(mailer.send({
+      from: 'from@email.com',
+      to: 'to@email.com',
+      subject: 'subject test',
+      message: '<p>message</p>'
+    })).rejects.toThrowError()
   })
 })
