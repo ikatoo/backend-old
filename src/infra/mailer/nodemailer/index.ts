903251c2e73ff7ccf88c836a446065c983805184
Milton Carlos Katoo <mckatoo@gmail.com>
2023-07-21 00:04:45 -0300

    test mailer service

--- src/infra/mailer/nodemailer/index.ts
+++ src/infra/mailer/nodemailer/index.ts
@@ -1,24 +1,28 @@
-import { IMail, Mail } from "@/repository/IMail";
+import { IMail, Mail, MailerResult } from "@/repository/IMail";
 import { env } from "@/utils/env";
 import nodemailer from "nodemailer";
 
 export default class NodeMailerImplementation implements IMail {
-  async send(mail: Mail): Promise<void> {
+  async send(mail: Mail): Promise<MailerResult> {
     const transporter = nodemailer.createTransport({
       host: env.SMTP_SERVER_ADDRESS,
       port: env.SMTP_SERVER_PORT,
-      secure: true,
+      secure: env.SMTP_SECURE,
       auth: {
         user: env.SMTP_USERNAME,
         pass: env.SMTP_PASSWORD
       }
     });
 
-    await transporter.sendMail({
+    const { response, ...info } = await transporter.sendMail({
       from: mail.from,
       to: mail.to,
       subject: mail.subject,
       html: mail.message
     });
+
+    const accepted = !!info.accepted.length && !info.rejected.length
+
+    return { accepted, response }
   }
 }
--- src/repository/IMail.ts
+++ src/repository/IMail.ts
@@ -9,6 +9,8 @@ const MailSchema = z.object({
 
 export type Mail = z.infer<typeof MailSchema>
 
+export type MailerResult = { accepted: boolean, response: string }
+
 export interface IMail {
-  send(mail: Mail): Promise<void>
+  send(mail: Mail): Promise<MailerResult>
 }
