243970187b0d6bf6dc8d600a8a063077ff6fe48f
Milton Carlos Katoo <mckatoo@gmail.com>
2023-07-25 16:02:02 -0300

    ♻️ (refactor): List of users without password

--- src/repository/IUser.ts
+++ src/repository/IUser.ts
@@ -14,13 +14,14 @@ export const UserSchema = z.object({
 export const PartialUserSchema = UserSchema.partial()
 
 export type User = z.infer<typeof UserSchema>
+export type UserWithoutPassword = Omit<User, 'password'>
 
 export default interface IUser {
   createUser(user: User): Promise<void>;
-  listUsers(): Promise<User[] | []>;
+  listUsers(): Promise<UserWithoutPassword[] | []>;
   getUserByID(id: number): Promise<User | undefined>
   getUserByEmail(email: Email): Promise<User | undefined>
-  findUsersByName(partialName: string): Promise<User[] | []>
+  findUsersByName(partialName: string): Promise<UserWithoutPassword[] | []>
   updateUser(id: number, user: Partial<User>): Promise<void>;
   deleteUser(id: number): Promise<void>;
   clear(): Promise<void>;
