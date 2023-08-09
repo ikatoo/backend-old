74e8fba6cc4cb41f68dafeff547de0abf45e0f2a
Milton Carlos Katoo <mckatoo@gmail.com>
2023-08-09 12:29:05 -0300

    removed array label

--- src/utils/transformers/getFieldsWithValues/index.ts
+++ src/utils/transformers/getFieldsWithValues/index.ts
@@ -4,7 +4,7 @@ export const getFieldsWithValues = <T>(page: Partial<T>) => {
   return Object.keys(page).map((field, index) => {
     const data = Object.values(page)[index];
     const stringData = (!!data && typeof data === 'object')
-      ? `{"${field}": ${JSON.stringify(data)}}`
+      ? JSON.stringify(data)
       : data;
 
     return `${toSnakeCase(field)} = '${stringData}'`;
