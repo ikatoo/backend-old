50a230bb7499fd00c929212b8dfd316fe2b240b8
Milton Carlos Katoo <mckatoo@gmail.com>
2023-08-09 18:19:23 -0300

    mocked user repository

--- src/infra/db/PgPromise/Users/UsersPgPromise/UsersRepository.test.ts
+++ src/infra/db/PgPromise/Users/UsersPgPromise/UsersRepository.test.ts
@@ -1,8 +1,11 @@
-vi.mock("@/utils/hasher")
+vi.mock('../..')
+vi.mock('@/utils/hasher', () => ({ hasher: vi.fn().mockResolvedValueOnce('hash') }))
+
 import { User } from "@/repository/IUser";
-import { beforeEach, describe, expect, test, vi } from "vitest";
+import { afterEach, beforeEach, describe, expect, test, vi } from "vitest";
 import UsersPgPromise from ".";
 import db from "../..";
+import { getFieldsWithValues } from "@/utils/transformers/getFieldsWithValues";
 
 describe("Basic operations in Users Repository", () => {
   const usersRepository = new UsersPgPromise()
@@ -22,101 +25,99 @@ describe("Basic operations in Users Repository", () => {
     value => `('${value.name}','${value.email}','${value.password}')`
   ).toString()
 
+  afterEach(() => {
+    vi.restoreAllMocks()
+    vi.clearAllMocks()
+  })
+
   beforeEach(() => {
     db.none('delete from users;')
   })
 
   test("CREATE Method", async () => {
-    vi.mock("@/utils/hasher", () => vi.fn().mockResolvedValueOnce('hash'))
-    
-    const mocked = vi.spyOn(db, 'none')
+    const mockedFn = vi.spyOn(db, 'none')
     const mock = mockedUsers[0]
-    await usersRepository.createUser(mock)
 
-    expect(mocked).toHaveBeenCalledTimes(1)
-    expect(mocked).toHaveBeenCalledWith(
+    await expect(usersRepository.createUser(mock)).resolves.not.toThrow()
+    expect(mockedFn).toHaveBeenCalledTimes(1)
+    expect(mockedFn).toHaveBeenCalledWith(
       'insert into users (name, email, password) values ($1, $2, $3)',
       [mock.name, mock.email, 'hash']
     )
-
-    // const result = await db.any('select name, email from users;')
-    // const expected = [{ name: mock.name, email: mock.email }]
-
-    // expect(result).toEqual(expected)
   });
 
   test("LIST ALL Method", async () => {
-    await db.none(
-      `insert into users (name, email, password) values ${values}`,
-      values
-    )
-
-    const results = (await usersRepository.listUsers())
-      .map(result => ({
-        name: result.name,
-        email: result.email
-      }))
-    const expected = mockedUsers.map(user => ({
-      name: user.name,
-      email: user.email
-    }))
+    const mockedData = [
+      { id: 1, name: 'name1', email: 'teste1@email.com' },
+      { id: 2, name: 'name2', email: 'teste2@email.com' },
+    ]
+    const spy = vi.spyOn(db, 'manyOrNone').mockResolvedValueOnce(mockedData)
+    const results = await usersRepository.listUsers()
+    const expected = mockedData
 
     expect(results).toEqual(expected)
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith('select id, name, email from users;')
   });
 
   test("GET BY EMAIL Method", async () => {
-    const mock = mockedUsers[1]
-    await db.none(
-      `insert into users (name, email, password) values ${values}`,
-      values
-    )
+    const mock = {
+      id: 1,
+      name: 'name1',
+      email: 'teste1@email.com',
+      password: 'hash'
+    }
+    const spy = vi.spyOn(db, 'oneOrNone').mockResolvedValueOnce(mock)
 
     const result = await usersRepository.getUserByEmail(mock.email)
 
-    expect(result).toEqual({ id: result?.id, ...mock })
+    expect(result).toEqual(mock)
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(
+      'select * from users where email=$1',
+      mock.email
+    )
   });
 
   test("FIND BY NAME Method", async () => {
-    const mock = mockedUsers[1]
-    await db.none(
-      `insert into users (name, email, password) values ${values}`,
-      values
-    )
+    const mock = [
+      { id: 2, name: 'name2', email: 'teste2@email.com' }
+    ]
+    const spy = vi.spyOn(db, 'manyOrNone').mockResolvedValueOnce(mock)
+
     const result = await usersRepository.findUsersByName('2')
-    const expected = [{
-      id: result[0].id,
-      name: mock.name,
-      email: mock.email
-    }]
+    const expected = mock
 
     expect(result).toEqual(expected)
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(
+      "select id, name, email from users where name like '%$1:value%';",
+      "2"
+    )
   });
 
   test("UPDATE Method", async () => {
-    await db.none(
-      `insert into users (name, email, password) values ${values}`,
-      values
+    const mock = mockedUsers[0]
+    const spy = vi.spyOn(db, 'none').mockResolvedValueOnce(null)
+    const fieldValues = getFieldsWithValues(mock).toString()
+
+    await expect(usersRepository.updateUser(7, mock))
+      .resolves.not.toThrow()
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(
+      'update users set $1:raw where id=$2;',
+      [fieldValues, 7]
     )
-    const existentUsers = await db.many('select * from users')
-    const updatedUser = { ...existentUsers[0], name: 'new name', password: 'newpass' }
-    await usersRepository.updateUser(updatedUser.id, updatedUser)
   });
 
   test("DELETE Method", async () => {
-    await db.none(
-      `insert into users (name, email, password) values ${values}`,
-      values
+    const spy = vi.spyOn(db, 'none').mockResolvedValueOnce(null)
+
+    await expect(usersRepository.deleteUser(9)).resolves.not.toThrow()
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(
+      'delete from users where id=$1',
+      9
     )
-
-    const userForDelete = await db.one('select * from users where email=$1', mockedUsers[0].email)
-    await usersRepository.deleteUser(userForDelete.id)
-
-    const expected = [mockedUsers[1]]
-    const result = (await db.many<User>('select * from users')).map(user => {
-      const { id, ...rest } = user
-      return rest
-    })
-
-    expect(result).toEqual(expected)
   })
 })
