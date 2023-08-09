2615176063c0f6b6147b6b00e7ff74426f2f2986
Milton Carlos Katoo <mckatoo@gmail.com>
2023-08-09 12:28:41 -0300

    📝 (docs): Add examples for use

--- src/utils/transformers/dateTransform/index.ts
+++ src/utils/transformers/dateTransform/index.ts
@@ -1,3 +1,7 @@
+/**
+ * @param stringDate Ex: '2023 - 06'
+ * @returns Ex: new Date(2023, 5)
+ */
 const stringToDate = (stringDate: string): Date => {
   const arrayDate = stringDate.split('-')
   const stringYear = arrayDate[0].split("")
@@ -9,6 +13,10 @@ const stringToDate = (stringDate: string): Date => {
   return new Date(year, monthIndex)
 }
 
+/**
+ * @param date Ex: new Date(2023, 5)
+ * @returns Ex: '2023 - 06'
+ */
 const dateToString = (date: Date): string => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -16,16 +24,28 @@ const dateToString = (date: Date): string => {
   return `${year} - ${month.toString().padStart(2, '0')}`
 }
 
+/**
+ * @param date Ex: 1685588400000
+ * @returns Ex: new Date(2023, 5)
+ */
 const numberToDate = (date: number): Date => {
   return new Date(date)
 }
 
+/**
+ * @param date Ex: new Date(2023, 5)
+ * @returns Ex: 1685588400000
+ */
 const dateToNumber = (date: Date): number => {
   const dateNumber = new Date(date.getFullYear(), date.getMonth()).getTime()
 
   return dateNumber
 }
 
+/**
+ * @param date Ex: '2023 - 06'
+ * @returns Ex: 1685588400000
+ */
 const dateStringToNumber = (date: string): number => {
   const arrayDate = date.split('-')
   const dateNumber = new Date(parseInt(arrayDate[0]), parseInt(arrayDate[1]) - 1).getTime()
@@ -33,6 +53,10 @@ const dateStringToNumber = (date: string): number => {
   return dateNumber
 }
 
+/**
+ * @param numberDate Ex: 1685588400000
+ * @returns Ex: '2023 - 06'
+ */
 const dateNumberToString = (numberDate: number): string => {
   const date = new Date(numberDate)
   const year = date.getFullYear()
