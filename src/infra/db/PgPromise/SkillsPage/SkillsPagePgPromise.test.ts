135c651a025f66dd89e6792805844e78bcdffd30
Milton Carlos Katoo <mckatoo@gmail.com>
2023-08-09 12:27:27 -0300

    mocked skills page repository

--- src/infra/db/PgPromise/SkillsPage/SkillsPagePgPromise.test.ts
+++ src/infra/db/PgPromise/SkillsPage/SkillsPagePgPromise.test.ts
@@ -1,5 +1,9 @@
+vi.mock('..')
+
+import { SkillsPage } from "@/repository/ISkillsPage";
+import { getFieldsWithValues } from "@/utils/transformers/getFieldsWithValues";
 import skillPageMock from "@shared/mocks/skillsPageMock/result.json";
-import { afterEach, describe, expect, test } from "vitest";
+import { afterEach, describe, expect, test, vi } from "vitest";
 import db from "..";
 import SkillsPagePgPromise from "./SkillsPagePgPromise";
 
@@ -8,64 +12,60 @@ describe("Basic operations in SkillsPage PgPromise Database", () => {
 
   afterEach(async () => {
     await db.none('delete from skills_page;')
+    vi.clearAllMocks()
   })
 
   test("CREATE Method", async () => {
+    const mockedFn = vi.spyOn(db, 'none')
+
     await expect(repository.createSkillsPage(skillPageMock))
       .resolves.not.toThrow()
+    expect(mockedFn).toHaveBeenCalledTimes(1)
+    expect(mockedFn).toHaveBeenCalledWith(
+      'insert into skills_page (title, description, skills, last_jobs) values ($1,$2,$3,$4);',
+      [
+        skillPageMock.title,
+        skillPageMock.description,
+        JSON.stringify(skillPageMock.skills),
+        JSON.stringify(skillPageMock.lastJobs)
+      ],
+    )
   });
 
   test("READ Method", async () => {
-    await repository.createSkillsPage(skillPageMock);
-    const actual = await repository.getSkillsPage();
+    const mockedFn = vi.spyOn(db, 'oneOrNone').mockResolvedValueOnce({
+      title: skillPageMock.title,
+      description: skillPageMock.description,
+      skills: skillPageMock.skills,
+      last_jobs: skillPageMock.lastJobs
+    })
+    const result = await repository.getSkillsPage()
+    const expected = skillPageMock
 
-    expect(skillPageMock).toEqual(actual);
+    expect(mockedFn).toHaveBeenCalledTimes(1)
+    expect(mockedFn).toHaveBeenCalledWith('select * from skills_page;')
+    expect(result).toEqual(expected)
   });
 
   test("UPDATE Method", async () => {
-    await repository.createSkillsPage(skillPageMock);
-    const newValue = {
-      description: "new description",
-      skills: [
-        {
-          skillTitle: 'skill 1',
-        },
-        {
-          skillTitle: 'skill 2',
-        },
-        {
-          skillTitle: 'skill 3',
-        },
-      ],
-      lastJobs: [
-        {
-          jobTitle: "new job 1",
-          jobDescription: "desc new job 1",
-          yearMonthStart: "2021 - 01",
-          yearMonthEnd: "2022 - 01",
-          link: "https://job1.com.br",
-        },
-        {
-          jobTitle: "new job 2",
-          jobDescription: "desc new job 2",
-          yearMonthStart: "2022 - 02",
-          yearMonthEnd: "2023 - 02",
-          link: "https://job2.com.br",
-        },
-      ]
-    };
-    await repository.updateSkillsPage(newValue);
-    const actual = await repository.getSkillsPage();
+    const mockedFn = vi.spyOn(db, 'none')
+    const fieldsWithValues = getFieldsWithValues<SkillsPage>(skillPageMock)
 
-    expect({ ...skillPageMock, ...newValue }).toEqual(actual);
+    await expect(repository.updateSkillsPage(skillPageMock)).resolves.not.toThrow()
+    expect(mockedFn).toHaveBeenCalledTimes(1)
+    expect(mockedFn).toHaveBeenCalledWith(
+      'update skills_page set $1:raw;',
+      [fieldsWithValues].toString()
+    )
   });
 
   test("DELETE Method", async () => {
-    await repository.createSkillsPage(skillPageMock);
-    await repository.deleteSkillsPage();
-    const expected = undefined;
-    const actual = await repository.getSkillsPage();
+    const mockedFn = vi.spyOn(db, 'none')
 
-    expect(expected).toEqual(actual);
+    await expect(repository.deleteSkillsPage()).resolves.not.toThrow()
+    expect(mockedFn).toHaveBeenCalledTimes(1)
+    expect(mockedFn).toHaveBeenCalledWith(
+      'delete from skills_page;'
+    )
   });
 });
