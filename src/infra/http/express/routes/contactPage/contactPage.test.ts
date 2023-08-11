fc1cfee6360a7b86fc2130dc8073f87652d0c402
Milton Carlos Katoo <mckatoo@gmail.com>
2023-08-10 23:39:27 -0300

    🤡 (tests): Mocked repository implementation on controller of the about and contact pages

--- src/infra/http/express/routes/contactPage/contactPage.test.ts
+++ src/infra/http/express/routes/contactPage/contactPage.test.ts
@@ -2,16 +2,15 @@ import { ContactPageRepository } from "@/infra/db";
 import { app } from "@/infra/http/express/server";
 import contactPageMock from "@shared/mocks/contactPageMock/result.json";
 import request from "supertest";
-import { afterEach, beforeEach, describe, expect, test } from "vitest";
+import { afterEach, describe, expect, test, vi } from "vitest";
 
 describe("/contact routes", () => {
   const repository = new ContactPageRepository();
 
-  beforeEach(async () => {
-  });
-
   afterEach(async () => {
-    await repository.clear()
+    vi.clearAllMocks()
+    vi.resetAllMocks()
+    vi.restoreAllMocks()
   });
 
   test("PUT Method: responds with 405 code when try use put method", async () => {
@@ -23,41 +22,59 @@ describe("/contact routes", () => {
   })
 
   test("GET Method: responds with data and 200 status code", async () => {
-    await repository.createContactPage(contactPageMock);
+    const spy = vi.spyOn(ContactPageRepository.prototype, 'getContactPage')
+      .mockResolvedValueOnce(contactPageMock)
+
     const { statusCode, body } = await request(app)
       .get("/contact")
       .send()
 
     expect(statusCode).toEqual(200)
     expect(body).toEqual(contactPageMock);
+    expect(spy).toHaveBeenCalledTimes(1)
   });
 
   test("GET Method: responds with 204 when not found data", async () => {
+    const spy = vi.spyOn(ContactPageRepository.prototype, 'getContactPage')
+      .mockResolvedValueOnce(undefined)
+
     const { statusCode, body } = await request(app)
       .get("/contact")
       .send()
 
     expect(statusCode).toBe(204);
     expect(body).toEqual({})
+    expect(spy).toHaveBeenCalledTimes(1)
   });
 
   test("POST Method: create contact page with 204 statusCode", async () => {
+    const spy = vi.spyOn(ContactPageRepository.prototype, 'createContactPage')
+      .mockResolvedValueOnce()
+
     const { statusCode } = await request(app)
       .post("/contact")
       .send(contactPageMock)
-    const page = await repository.getContactPage()
 
     expect(statusCode).toBe(201);
-    expect(page).toEqual(contactPageMock)
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(contactPageMock)
   });
 
   test("POST Method: responds with 409 when try create page with existent data", async () => {
-    await repository.createContactPage(contactPageMock);
-    const { statusCode } = await request(app)
+    const spy = vi.spyOn(ContactPageRepository.prototype, 'createContactPage')
+      .mockImplementationOnce(() => {
+        throw new Error("duplicate");
+      })
+
+    const { body, statusCode } = await request(app)
       .post("/contact")
       .send(contactPageMock)
 
     expect(statusCode).toBe(409);
+    expect(body).toHaveProperty('message')
+    expect(body.message).toEqual('Duplicated data.')
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(contactPageMock)
   })
 
   test("POST Method: responds with 400 when request without payload", async () => {
@@ -69,15 +86,20 @@ describe("/contact routes", () => {
   })
 
   test("PATCH Method: responds with 204 when update", async () => {
+    const mockedData = { title: 'new title' }
+    const spy = vi.spyOn(ContactPageRepository.prototype, 'updateContactPage')
+      .mockResolvedValueOnce()
+
     const { statusCode } = await request(app)
       .patch("/contact")
-      .send({ title: 'new title' })
+      .send(mockedData)
 
     expect(statusCode).toBe(204);
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(mockedData)
   });
 
   test("PATCH Method: responds with 409 when try update with invalid payload", async () => {
-    await repository.createContactPage(contactPageMock);
     const { statusCode } = await request(app)
       .patch("/contact")
       .send({ invalid: 'payload' })
@@ -94,14 +116,14 @@ describe("/contact routes", () => {
   });
 
   test("DELETE Method: responde with status 204", async () => {
-    await repository.createContactPage(contactPageMock);
+    const spy = vi.spyOn(ContactPageRepository.prototype, 'deleteContactPage')
+      .mockResolvedValueOnce()
+
     const { statusCode } = await request(app)
       .delete("/contact")
       .send()
-    const actual = await repository.getContactPage()
 
     expect(statusCode).toBe(204)
-    expect(actual).toBeUndefined()
+    expect(spy).toHaveBeenCalledTimes(1)
   })
-
 });
