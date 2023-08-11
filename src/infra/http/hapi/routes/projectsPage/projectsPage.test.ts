a6d77c49499ca21ae22471bf1852d317de2c440c
Milton Carlos Katoo <mckatoo@gmail.com>
2023-08-11 17:26:25 -0300

    mocked repositories

--- src/infra/http/hapi/routes/projectsPage/projectsPage.test.ts
+++ src/infra/http/hapi/routes/projectsPage/projectsPage.test.ts
@@ -2,11 +2,12 @@ import { ProjectsRepository } from "@/infra/db";
 import { ProjectWithId } from "@/repository/IProject";
 import { Server, ServerApplicationState } from "@hapi/hapi";
 import projectsPageMock from "@shared/mocks/projectsMock/result.json";
-import { afterEach, beforeEach, describe, expect, test } from "vitest";
+import { afterEach, beforeEach, describe, expect, test, vi } from "vitest";
 import { init } from "../../server";
 
 describe("/project routes", () => {
-  const repository = new ProjectsRepository()
+  const mockWithID = projectsPageMock.map((project, id) => ({ id, ...project }))
+
   let server: Server<ServerApplicationState>;
 
   beforeEach(async () => {
@@ -15,7 +16,8 @@ describe("/project routes", () => {
 
   afterEach(async () => {
     await server.stop();
-    await repository.clear()
+    vi.clearAllMocks()
+    vi.restoreAllMocks()
   });
 
   test("PUT Method: responds with 405 code when try use put method", async () => {
@@ -28,42 +30,52 @@ describe("/project routes", () => {
   })
 
   test("GET Method: result is equal the mock with 200 statusCode", async () => {
-    await repository.createProject(projectsPageMock[1]);
+    const spy = vi.spyOn(ProjectsRepository.prototype, 'getProjects')
+      .mockResolvedValueOnce(mockWithID)
+
     const { result, statusCode } = await server.inject<ProjectWithId[]>({
       method: "get",
       url: "/projects",
     });
-    const { id, ...actual } = (result as ProjectWithId[])[0]
 
     expect(statusCode).toBe(200);
-    expect(actual).toEqual(projectsPageMock[1]);
-    expect(result).toHaveLength(1)
+    expect(result).toEqual(mockWithID)
+    expect(spy).toHaveBeenCalledTimes(1)
   });
 
   test("GET Method: responds with 204 when not found data", async () => {
-    const { statusCode } = await server.inject({
+    const spy = vi.spyOn(ProjectsRepository.prototype, 'getProjects')
+      .mockResolvedValueOnce([])
+
+    const { statusCode, result } = await server.inject({
       method: "get",
       url: "/projects",
     });
 
     expect(statusCode).toBe(204);
+    expect(result).toEqual(null)
+    expect(spy).toHaveBeenCalledTimes(1)
   })
 
   test("POST Method: create project with 204 statusCode", async () => {
+    const spy = vi.spyOn(ProjectsRepository.prototype, 'createProject')
+      .mockResolvedValueOnce()
+
     const { statusCode } = await server.inject({
       method: "post",
       url: "/project",
       payload: projectsPageMock[0]
     });
-    const title = projectsPageMock[0].description.title
-    const projects = await repository.getProjectsByTitle(title) as ProjectWithId[]
-    const { id, ...actual } = projects[0]
+
     expect(statusCode).toBe(201);
-    expect(actual).toEqual(projectsPageMock[0])
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(projectsPageMock[0])
   });
 
   test("POST Method: responds with 409 when try create page with existent data", async () => {
-    await repository.createProject(projectsPageMock[1]);
+    const spy = vi.spyOn(ProjectsRepository.prototype, 'createProject')
+      .mockImplementationOnce(() => { throw new Error('duplicate') })
+
     const { statusCode } = await server.inject({
       method: "post",
       url: "/project",
@@ -71,6 +83,8 @@ describe("/project routes", () => {
     });
 
     expect(statusCode).toBe(409);
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(projectsPageMock[1])
   })
 
   test("POST Method: responds with 400 when request without payload", async () => {
@@ -83,25 +97,26 @@ describe("/project routes", () => {
   })
 
   test("PATCH Method: responds with 204 when update", async () => {
-    await repository.createProject(projectsPageMock[0]);
-    const projects = await repository.getProjectsByTitle(projectsPageMock[0].description.title) as ProjectWithId[]
-    const { id } = projects[0]
+    const mockedID = 8
+    const mockedData = { description: { title: 'new title' } }
+    const spy = vi.spyOn(ProjectsRepository.prototype, 'updateProject')
+      .mockResolvedValueOnce()
+
     const res = await server.inject({
       method: "patch",
-      url: `/project/id/${id}`,
-      payload: { description: { title: 'new title' } }
+      url: `/project/id/${mockedID}`,
+      payload: mockedData
     });
 
     expect(res.statusCode).toBe(204);
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(mockedID, mockedData)
   });
 
   test("PATCH Method: responds with 409 when try update with invalid payload", async () => {
-    await repository.createProject(projectsPageMock[0]);
-    const projects = await repository.getProjectsByTitle(projectsPageMock[0].description.title) as ProjectWithId[]
-    const { id } = projects[0]
     const res = await server.inject({
       method: "patch",
-      url: `/project/id/${id}`,
+      url: `/project/id/8`,
       payload: { invalid: 'payload' }
     });
 
@@ -109,73 +124,88 @@ describe("/project routes", () => {
   });
 
   test("PATCH Method: responds with 400 when try update without payload", async () => {
-    await repository.createProject(projectsPageMock[0]);
-    const projects = await repository.getProjectsByTitle(projectsPageMock[0].description.title) as ProjectWithId[]
-    const { id } = projects[0]
     const res = await server.inject({
       method: "patch",
-      url: `/project/id/${id}`,
+      url: `/project/id/90`,
     });
 
     expect(res.statusCode).toBe(400);
   });
 
   test("DELETE Method: responds with 204", async () => {
-    await repository.createProject(projectsPageMock[0]);
-    const projects = await repository.getProjectsByTitle(projectsPageMock[0].description.title) as ProjectWithId[]
-    const { id } = projects[0]
+    const spy = vi.spyOn(ProjectsRepository.prototype, 'deleteProject')
+      .mockResolvedValueOnce()
+
     const res = await server.inject({
       method: "delete",
-      url: `/project/id/${id}`,
+      url: `/project/id/88`,
     });
 
     expect(res.statusCode).toBe(204);
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(88)
   });
 
   test("GET Method: get project with id", async () => {
-    await repository.createProject(projectsPageMock[0]);
-    const projects = await repository.getProjectsByTitle(projectsPageMock[0].description.title) as ProjectWithId[]
-    const { id } = projects[0]
+    const mockedData = mockWithID[1]
+    const spy = vi.spyOn(ProjectsRepository.prototype, 'getProjectById')
+      .mockResolvedValueOnce(mockedData)
+
     const { result, statusCode } = await server.inject({
       method: "get",
-      url: `/project/id/${id}`,
+      url: `/project/id/${mockedData.id}`,
     });
 
     expect(statusCode).toBe(200);
-    expect(result).toEqual({ id, ...projectsPageMock[0] });
+    expect(result).toEqual(mockedData);
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(mockedData.id)
   });
 
   test("GET Method: should return statusCode 404 when project with id not exist", async () => {
+    const spy = vi.spyOn(ProjectsRepository.prototype, 'getProjectById')
+      .mockResolvedValueOnce(undefined)
+
     const { statusCode } = await server.inject({
       method: "get",
       url: '/project/id/1000000',
     });
 
     expect(statusCode).toBe(404);
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(1000000)
   });
 
   test("GET Method: get project with like title", async () => {
-    await repository.createProject(projectsPageMock[0]);
-    const { title } = projectsPageMock[0].description
+    const mockedTitle = 'mocked_title'
+    const spy = vi.spyOn(ProjectsRepository.prototype, 'getProjectsByTitle')
+      .mockResolvedValueOnce(mockWithID)
+
     const { result, statusCode } = await server.inject({
       method: "get",
-      url: `/projects/title/${title}`,
+      url: `/projects/title/${mockedTitle}`,
     });
-    const { id, ...actual } = (result as ProjectWithId[])[0]
 
     expect(statusCode).toBe(200);
-    expect(actual).toEqual(projectsPageMock[0]);
+    expect(result).toEqual(mockWithID)
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(mockedTitle)
   });
 
   test("GET Method: should return statusCode 204 when not found data with this title", async () => {
-    await repository.createProject(projectsPageMock[0]);
+    const mockedTitle = 'sdfsdf'
+    const spy = vi.spyOn(ProjectsRepository.prototype, 'getProjectsByTitle')
+      .mockResolvedValueOnce(undefined)
+
     const { result, statusCode } = await server.inject({
       method: "get",
-      url: '/projects/title/sdfsdf',
+      url: `/projects/title/${mockedTitle}`,
     });
 
     expect(statusCode).toBe(204);
     expect(result).toBeNull();
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy).toHaveBeenCalledWith(mockedTitle)
   });
 
 });
