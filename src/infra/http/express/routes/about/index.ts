0e33d3a48f47a5dc33ad0a13f2db62e1f1a99543
Milton Carlos Katoo <mckatoo@gmail.com>
2023-08-15 12:45:33 -0300

    verify token on aboutPage routes

--- src/infra/http/express/routes/about/index.ts
+++ src/infra/http/express/routes/about/index.ts
@@ -1,6 +1,7 @@
 import { expressAdapter } from '@/infra/http/adapters/expressAdapter'
 import { createAboutPageHandler, deleteAboutPageHandler, getAboutPageHandler, updateAboutPageHandler } from '@/infra/http/controllers/aboutPage/aboutPageController'
 import { Router } from 'express'
+import verifyTokenMiddleware from '../../middlewares/verifyTokenMiddleware'
 
 const aboutRoutes = Router()
 
@@ -18,16 +19,19 @@ aboutRoutes.get(
 
 aboutRoutes.post(
   '/about',
+  verifyTokenMiddleware,
   expressAdapter(createAboutPageHandler)
 )
 
 aboutRoutes.patch(
   '/about',
+  verifyTokenMiddleware,
   expressAdapter(updateAboutPageHandler)
 )
 
 aboutRoutes.delete(
   '/about',
+  verifyTokenMiddleware,
   expressAdapter(deleteAboutPageHandler)
 )
 
