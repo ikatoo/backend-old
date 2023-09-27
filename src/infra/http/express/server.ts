d22abd6dce2aa7228d0cec0c77e18dce82265aa4
Milton Carlos Katoo <mckatoo@gmail.com>
2023-09-27 20:12:08 -0300

    installed swagger

--- src/infra/http/express/server.ts
+++ src/infra/http/express/server.ts
@@ -6,6 +6,9 @@ import morgan from 'morgan';
 import { errorMiddleware } from './middlewares/errorMiddleware';
 import { routes } from './routes';
 
+import swaggerDoc from '@/infra/docs/swagger.json';
+import swaggerUi from 'swagger-ui-express';
+
 const app = express()
 
 env.NODE_ENV.toLowerCase().startsWith('dev')
@@ -13,6 +16,9 @@ env.NODE_ENV.toLowerCase().startsWith('dev')
   : app.use(morgan('combined'))
 
 app.use(express.json())
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+
 app.use(express.urlencoded({ extended: true }))
 
 app.disable('x-powered-by').disable('etag')
